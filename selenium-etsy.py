2f0ed18e44e6d44e71792bdddd8ff4de7a5c710f
yoniblumenfeld <yonei06@gmail.com>
2018-07-08 22:02:36 +0300

    finished etsy

--- selenium-etsy.py
+++ selenium-etsy.py
@@ -18,7 +18,8 @@ class Etsy:
         """
         description_text_element = shoes_element.find_element_by_xpath(".//p[@class='text-gray text-truncate mb-xs-0 text-body']")
         if re.search("vintage", description_text_element.text, re.IGNORECASE) != None:
-            self.vintage_shoes_list.append(shoes_element.get_attribute("href"))
+            if not(shoes_element.get_attribute("href") in self.vintage_shoes_list):
+                self.vintage_shoes_list.append(shoes_element.get_attribute("href"))
 
 
     def find_vintage_out_of_main_shoes(self,driver):
@@ -27,10 +28,28 @@ class Etsy:
         it calls the map function to run the add_if_shoes_vintage function on each product element.
         """
         all_shoes = WebDriverWait(driver=driver, timeout=7).until(
-            EC.presence_of_all_elements_located((By.XPATH,"//a[@data-listing-id and @data-palette-listing-image and @data-logging-key and @title]"))
+            EC.presence_of_all_elements_located((By.XPATH,"//a[@data-listing-id and @data-logging-key and @title]"))
         )
         map(lambda shoes: self.add_if_shoes_vintage(driver,shoes),all_shoes)
 
+    def search_for_shoes(self,driver):
+        """
+        this method searches for shoes in the search-bar.
+        it waits for the search bar element to be located and interacts with it.
+        """
+
+        search_input = WebDriverWait(driver=driver, timeout=10).until(
+            EC.presence_of_element_located((By.ID, "search-query"))
+        )  # waiting for search input element to be loaded
+        search_input.send_keys("shoes")  # searches shoes
+        search_input.submit()  # submits the search
+
+    def sort_vintage_by_radio(self,driver):
+        """
+        checking the Vintage item-type checkbutton. thus, causing only vintage items to appear.
+        """
+        vintage_item_type_radio_btn = driver.find_element_by_xpath("//a[contains(@data-context,'item_type') and contains(text(),'Vintage')]")
+        vintage_item_type_radio_btn.click()
 
     def test_flow(self):
         """
@@ -39,15 +58,13 @@ class Etsy:
         """
         driver = self.driver
         driver.get("https://www.etsy.com/")
-        search_input = WebDriverWait(driver=driver,timeout=10).until(
-            EC.presence_of_element_located((By.ID,"search-query"))
-        ) #waiting for search input element to be loaded
+        self.search_for_shoes(driver)
+        self.find_vintage_out_of_main_shoes(driver)
+        print self.vintage_shoes_list #the shoes found without checking the Vintage item-type checkbox
+        self.sort_vintage_by_radio(driver)
+        self.find_vintage_out_of_main_shoes(driver)
+        print self.vintage_shoes_list #the shoes found after checking the vintage item-type checkbox
 
-        search_input.send_keys("shoes") #searches shoes
-        search_input.submit() #submits the search
-
-        self.find_vintage_out_of_main_shoes(driver) #finding vintage shoes using product description text
-        print self.vintage_shoes_list
         time.sleep(30)
 
 
